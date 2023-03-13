1e5c853aa439ab67e94b0d66ed30a77cc6bb8432
Brocha05 <brocha_05@hotmail.com>
2023-03-13 10:44:23 -0600

    Serving static content

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({//Servir contenido estático, nos permite regresar contenido HTML
+      //o aplicaciones en React/Angular/Vue, se tiene que installar el módulo @nestjs/serve-static
+      //y añadir esta configuración en los imports del módulo principal.
+      rootPath: join(__dirname, '..', 'public'),
+    })
+  ],
 })
 export class AppModule {}
