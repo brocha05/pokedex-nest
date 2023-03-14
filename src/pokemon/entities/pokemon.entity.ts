948123e35c574cf43221db6247155fbf5563aaac
Brocha05 <brocha_05@hotmail.com>
2023-03-14 15:11:57 -0600

    Creating models and schemas

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,21 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";//Se importa document para heredarlo en la clase 
+
+@Schema()//Decorador propio de Nest que indica que es un esquema de base de datos esta entidad.
+export class Pokemon extends Document{//Document permite añadir las características de un documento
+    //de mongo a mi entidad y así poderla manipular más facilmente.
+    @Prop({
+        unique: true,
+        index: true
+    })
+    name: string;
+
+    @Prop({
+        unique: true,
+        index: true
+    })
+    no: number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon)//Se debe exportar el esquema para 
+//Cuando se genere la base de datos los detecte  y cree
--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { PokemonController } from './pokemon.controller';
+import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   controllers: [PokemonController],
-  providers: [PokemonService]
+  providers: [PokemonService],
+  imports: [
+    MongooseModule.forFeature([//Importamos un módulo en el que pasamos como argumento la clase
+    //Y el nombre del esquema para crear la colección en mongo
+      {
+        name: Pokemon.name,
+        schema: PokemonSchema,
+      },
+    ]),
+  ],
 })
 export class PokemonModule {}
