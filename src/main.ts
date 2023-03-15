9aba4bd58854d3a32c220d3de5519abea3dffc8c
Brocha05 <brocha_05@hotmail.com>
2023-03-15 08:57:01 -0600

    POST validating data

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api')//Agrega el prefijo a los paths de la API Rest cuando se quiera hacer fetch.
+  app.setGlobalPrefix('api/v2'); //Agrega el prefijo a los paths de la API Rest cuando se quiera hacer fetch.
   await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 }
 bootstrap();
--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -3,6 +3,7 @@ import { PokemonService } from './pokemon.service';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 
+
 @Controller('pokemon')
 export class PokemonController {
   constructor(private readonly pokemonService: PokemonService) {}
