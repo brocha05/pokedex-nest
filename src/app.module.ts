23f06dfbac5847a33f6c11dd6d3e547d8f29c080
Brocha05 <brocha_05@hotmail.com>
2023-03-17 08:15:56 -0600

    Creating the seed modulo and installing axios

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
@@ -14,7 +15,7 @@ import { CommonModule } from './common/common.module';
     }),
     MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),//Conexión a la base de datos
     PokemonModule, 
-    CommonModule, 
+    CommonModule, SeedModule, 
   ],
 })
 export class AppModule {}
